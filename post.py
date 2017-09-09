65f5ee24aa8a0c66ff75e02bc59084317f8a8c45
Matheus Gaudencio <matheusgr@gmail.com>
2017-09-09 20:30:14 -0300

    Add bash example.

--- post.py
+++ post.py
@@ -9,12 +9,12 @@ import json
 def log(type_, data):
     url = 'http://localhost:4020/api/action/' + type_
     urlrequest.urlopen(url, data=json.dumps(data).encode())
-    
+
 
 def get_actions():
     url = 'http://localhost:4020/api/action/?key=bananinha'
     return json.loads(urlrequest.urlopen(url).read())
 
 
-log("teste", {'albo':[123,1234]})
+log("test", {'something': [123, 1234]})
 print(get_actions())
